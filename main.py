3138e5393643fe35b73a74c4c4615d4684887eaf
Mahdi Esmailoghli <masmaillogli@gmail.com>
2017-04-07 18:06:31 +0430

    Update main.py

--- main.py
+++ main.py
@@ -181,8 +181,8 @@ print("final accuracy on test set: %s" %str(sess.run(accuracy_OP, feed_dict={X: 
 
 
 for j in range(104):
-    print(" Pobabilities of object %d :  %s" %(j,str(sess.run(activation_OP, feed_dict={X: testX[[j],[1]]}))))
-    print("True label is: %s" %str(testY[[j],:]))
+    print(" Pobabilities of object %d :  %s" %(j,str(sess.run(activation_OP, feed_dict={X: testX[[j],:]}))))
+    print("True label is: %s" %str(testY[[j],[1]]))
 
 ##############################
 ### SAVE TRAINED VARIABLES ###
