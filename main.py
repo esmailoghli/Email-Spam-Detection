9ba0a9786b16ceecd8396eef32d4d62a2ca6515f
Mahdi Esmailoghli <masmaillogli@gmail.com>
2017-04-09 14:34:24 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-################
-### PREAMBLE ###
-################
-
 from __future__ import division
 import tensorflow as tf
 import numpy as np
@@ -40,59 +36,31 @@ trainX,trainY,testX,testY = import_data()
 ### GLOBAL PARAMETERS ###
 #########################
 
-# DATA SET PARAMETERS
-# Get our dimensions for our different variables and placeholders:
-# numFeatures = the number of words extracted from each email
 numFeatures = trainX.shape[1]
-# numLabels = number of classes we are predicting (here just 2: Ham or Spam)
 numLabels = trainY.shape[1]
 
-# TRAINING SESSION PARAMETERS
-# number of times we iterate through training data
-# tensorboard shows that accuracy plateaus at ~25k epochs
-numEpochs = 27000
-# a smarter learning rate for gradientOptimizer
-learningRate = tf.train.exponential_decay(learning_rate=0.0008,
-                                          global_step= 1,
-                                          decay_steps=trainX.shape[0],
-                                          decay_rate= 0.95,
-                                          staircase=True)
+numEpochs = 5000
+learningRate = 0.01
+
 ####################
 ### PLACEHOLDERS ###
 ####################
 
-# X = X-matrix / feature-matrix / data-matrix... It's a tensor to hold our email
-# data. 'None' here means that we can hold any number of emails
 X = tf.placeholder(tf.float32, [None, numFeatures])
-# yGold = Y-matrix / label-matrix / labels... This will be our correct answers
-# matrix. Every row has either [1,0] for SPAM or [0,1] for HAM. 'None' here 
-# means that we can hold any number of emails
 yGold = tf.placeholder(tf.float32, [None, numLabels])
 
 #################
 ### VARIABLES ###
 #################
 
-# Values are randomly sampled from a Gaussian with a standard deviation of:
-#     sqrt(6 / (numInputNodes + numOutputNodes + 1))
-
-weights = tf.Variable(tf.random_normal([numFeatures,numLabels],
-                                       mean=0,
-                                       stddev=(np.sqrt(6/numFeatures+
-                                                         numLabels+1)),
-                                       name="weights"))
-bias = tf.Variable(tf.random_normal([1,numLabels],
-                                    mean=0,
-                                    stddev=(np.sqrt(6/numFeatures+numLabels+1)),
-                                    name="bias"))
+weights = tf.Variable(tf.zeros([numFeatures,numLabels])
+bias = tf.Variable(tf.zeros([numFeatures,numLabels])
 ######################
 ### PREDICTION OPS ###
 ######################
 
-# INITIALIZE our weights and biases
 init_OP = tf.global_variables_initializer()
 
-# PREDICTION ALGORITHM i.e. FEEDFORWARD ALGORITHM
 apply_weights_OP = tf.matmul(X, weights, name="apply_weights")
 activation_OP = tf.nn.softmax(tf.add(apply_weights_OP, bias, name="add_bias"))
 
@@ -100,49 +68,26 @@ activation_OP = tf.nn.softmax(tf.add(apply_weights_OP, bias, name="add_bias"))
 ### EVALUATION OP ###
 #####################
 
-# COST FUNCTION i.e. MEAN SQUARED ERROR
 cost_OP = tf.nn.l2_loss(activation_OP-yGold, name="squared_error_cost")
 
 #######################
 ### OPTIMIZATION OP ###
 #######################
 
-# OPTIMIZATION ALGORITHM i.e. GRADIENT DESCENT
 training_OP = tf.train.GradientDescentOptimizer(learningRate).minimize(cost_OP)
 
 #####################
 ### RUN THE GRAPH ###
 #####################
-epoch_values=[]
 accuracy_values=[]
 cost_values=[]
-# Create a tensorflow session
-sess = tf.Session()
 
-# Initialize all tensorflow variables
+sess = tf.Session()
 sess.run(init_OP)
 
-## Ops for vizualization
-# argmax(activation_OP, 1) gives the label our model thought was most likely
-# argmax(yGold, 1) is the correct label
 correct_predictions_OP = tf.equal(tf.argmax(activation_OP,1),tf.argmax(yGold,1))
-# False is 0 and True is 1, what was our average?
 accuracy_OP = tf.reduce_mean(tf.cast(correct_predictions_OP, "float"))
-# Summary op for regression output
-activation_summary_OP = tf.summary.histogram("output", activation_OP)
-# Summary op for accuracy
-accuracy_summary_OP = tf.summary.scalar("accuracy", accuracy_OP)
-# Summary op for cost
-cost_summary_OP = tf.summary.scalar("cost", cost_OP)
-# Summary ops to check how variables (W, b) are updating after each iteration
-weightSummary = tf.summary.histogram("weights", weights.eval(session=sess))
-biasSummary = tf.summary.histogram("biases", bias.eval(session=sess))
-# Merge all summaries
-all_summary_OPS = tf.summary.merge_all()
-# Summary writer
-writer = tf.summary.FileWriter("summary_logs", sess.graph)
 
-# Initialize reporting variables
 cost = 0
 diff = 1
 
@@ -156,14 +101,10 @@ for i in range(numEpochs):
         step = sess.run(training_OP, feed_dict={X: trainX, yGold: trainY})
         # Report occasional stats
         if i % 10 == 0:
-            # Add epoch to epoch_values
-            epoch_values.append(i)
-            # Generate accuracy stats on test data
             summary_results, train_accuracy, newCost = sess.run(
                 [all_summary_OPS, accuracy_OP, cost_OP], 
                 feed_dict={X: trainX, yGold: trainY}
             )
-            # Add accuracy to live graphing variable
             accuracy_values.append(train_accuracy)
             # Add cost to live graphing variable
             cost_values.append(newCost)
@@ -172,11 +113,9 @@ for i in range(numEpochs):
             # Re-assign values for variables
             diff = abs(newCost - cost)
             cost = newCost
-            #generate print statements
             print("step %d, training accuracy %g"%(i, train_accuracy))
             print("step %d, cost %g"%(i, newCost))
             print("step %d, change in cost %g"%(i, diff))
-# How well do we perform on held-out test data?
 print("final accuracy on test set: %s" %str(sess.run(accuracy_OP, feed_dict={X: testX, yGold: testY})))
 
 for j in range(104):
@@ -188,8 +127,4 @@ for j in range(104):
 
 # Create Saver
 saver = tf.train.Saver()
-# Save variables to .ckpt file
-# saver.save(sess, "trained_variables.ckpt")
-
-# Close tensorflow session
 sess.close()
